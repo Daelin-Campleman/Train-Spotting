77e3550655ea0bb36d0ae12f282362cc6ec390f9
Daelin Campleman <daelin.campleman@bbd.co.za>
2023-07-18 13:33:31 +0200

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -6,6 +6,21 @@ app.use(express.static('public'));
 var server = app.listen(8080, function () {
    var host = server.address().address
    var port = server.address().port
-   
+
    console.log("Frontend listening at http://%s:%s", host, port)
 })
+
+const https = require("https");
+const fs = request("fs");
+
+const express = require("express");
+
+const app = express();
+
+https
+   .createServer(app)
+   .listen(8080, () => {
+      console.log('server is runing at port 8080')
+   });
+
+app.use(express.static('public'));
