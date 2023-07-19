18db9c39c4705e8d7db5dc29b9ba87a267da1aaa
Daelin Campleman <127960675+Daelin-Campleman@users.noreply.github.com>
2023-07-19 17:20:50 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ sudo docker run -d --net host --name TrainSpotting-Frontend --restart unless-sto
 
 cd ../backend;
 
-mvn clean package;
+/opt/apache-maven-3.9.3/bin/mvn clean package;
 
 sudo docker build -t backend .;
 sudo docker run -d --net host --name TrainSpotting-Backend --restart unless-stopped -tld backend;
