2e04a6420fecd84ef0d4173989e6be58facc6adc
Daelin Campleman <127960675+Daelin-Campleman@users.noreply.github.com>
2023-07-20 12:39:08 +0200

    Update DockerMessageController.java

--- backend/src/main/java/com/train/TrainSpotting/DockerMessageController.java
+++ backend/src/main/java/com/train/TrainSpotting/DockerMessageController.java
@@ -1,4 +1,4 @@
-package com.baeldung.spring.cloud.config.docker;
+package com.train.TrainSpotting;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
