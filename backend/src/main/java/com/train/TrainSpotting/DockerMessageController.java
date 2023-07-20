d95ab801e92e6d0ce2cb424fcfae0230a4defa0c
Daelin Campleman <daelin.campleman@bbd.co.za>
2023-07-20 13:01:38 +0200

    Update DockerMessageController.java

--- backend/src/main/java/com/train/TrainSpotting/DockerMessageController.java
+++ backend/src/main/java/com/train/TrainSpotting/DockerMessageController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DockerMessageController {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String home(){
         return "Train Spotting API base URL";
     }
