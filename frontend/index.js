82424a8a6b47caf138e1d8fe12f90d34147cc049
Marni Kleingeld <marni@bbd.co.za>
2023-07-24 11:57:05 +0200

    added OAuth2 & login screen

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,89 @@
-const express = require('express')
-const app = express()
-const port = 8080
+require("dotenv").config();
+const express = require("express");
+const passport = require("passport");
+const session = require("express-session");
+require("./passport");
 
-app.use(express.static('public'));
+const app = express();
+const port = process.env.PORT || 8080;
+const path = require("path");
+
+app.use(express.static("public"));
+
+// express session
+app.use(
+  session({
+    secret: process.env.secret,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Middleware used in protected routes to check if the user has been authenticated
+const isLoggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+// Base route
+app.get("/", (req, res) => {
+  res.redirect("/login");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/login/login.html"));
+});
+
+// Google Auth consent screen route
+app.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+  })
+);
+
+// Call back route
+app.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failed",
+  }),
+  function (req, res) {
+    res.redirect("/success");
+  }
+);
+
+// failed route if the authentication fails
+app.get("/failed", (req, res) => {
+  console.log("User is not authenticated");
+  res.send("Failed");
+});
+
+// Success route if the authentication is successful
+app.get("/success", isLoggedIn, (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// Route that logs out the authenticated user
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log("Error while destroying session:", err);
+    } else {
+      req.logout(() => {
+        console.log("You are logged out");
+        res.redirect("/");
+      });
+    }
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
